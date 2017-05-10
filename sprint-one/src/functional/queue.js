8a63f9a8c158bdde1fa2b287dbcffd2b327e52bb
Hack Reactor Student <student@hackreactor.com>
2017-05-10 12:16:53 -0500

    Implement final test for functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -14,15 +14,21 @@ var Queue = function() {
   };
 
   someInstance.dequeue = function() {
+    console.log('top storage', storage);
     if (someInstance.items === 0) {
       return 0;
     } else {
       var dequeued = storage[0];
+      delete storage[0];
       for (var key in storage) {
-        storage[key] = storage[key + 1];
-        delete storage[someInstance.size()];
+        console.log('key', key);
+        if (storage[key] !== undefined) {
+          storage[key - 1] = storage[key];
+        }
       }
+      delete storage[someInstance.size() - 1];
       someInstance.items--;
+      console.log('storage', storage);
       return dequeued;
     }
   };
