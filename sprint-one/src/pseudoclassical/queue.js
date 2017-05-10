52b6d517839dd6c0e60df6cbc06d108516de1933
Hack Reactor Student <student@hackreactor.com>
2017-05-10 16:14:47 -0500

    Implement queue and stack with pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.items = 0;
+  
+};
+
+Queue.prototype.size = function() {
+  return this.items;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.items++;
+  this.storage[this.size() - 1] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.items === 0) {
+    return 0;
+  } else {
+    var dequeued = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      if (this.storage[key] !== undefined) {
+        this.storage[key - 1] = this.storage[key];
+      }
+    }
+    this.items--;
+    return dequeued;
+  }
+  return this.items;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  this.storage = {};
+  this.items = 0;
+  
 };
 
+Stack.prototype.size = function() {
+  return this.items;
+};
 
+Stack.prototype.push = function(value) {
+  this.items++;
+  this.storage[this.size() - 1] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.items === 0) {
+    return 0;
+  } else {
+    var popped = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    this.items--;
+    return popped;
+  }
+  return this.items;
+};
