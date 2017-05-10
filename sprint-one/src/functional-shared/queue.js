ba862dd0f958abbb6776f2ff7c4c45dad3998860
Hack Reactor Student <student@hackreactor.com>
2017-05-10 15:20:47 -0500

    Implement queue with functional shared instantiation pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,41 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.items = 0;
+  someInstance.size = queueMethods.size;
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  
+  // _.extend(someInstance, Stack.methods);
+  
+  return someInstance;
 };
 
 var queueMethods = {};
 
-
+queueMethods.size = function() {
+  return this.items;
+};
+queueMethods.enqueue = function(value) {
+  this.items++;
+  this.storage[this.size() - 1] = value;
+};
+queueMethods.dequeue = function() {
+  if (this.items === 0) {
+    return 0;
+  } else {
+    var dequeued = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      if (this.storage[key] !== undefined) {
+        this.storage[key - 1] = this.storage[key];
+      }
+    }
+    this.items--;
+    return dequeued;
+  }
+  return this.items;
+};
