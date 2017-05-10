38c8ba07efc84193f1a82c0edf0966911a1f9f99
Hack Reactor Student <student@hackreactor.com>
2017-05-10 11:44:49 -0500

    Implement functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,33 @@
-var Queue = function() {
-  var someInstance = {};
+var Stack = function() {
+  var someInstance = {
+    'items' : 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // var test = someInstance.size().toString();
 
   // Implement the methods below
-
-  someInstance.enqueue = function(value) {
+  someInstance.push = function(value) {
+    someInstance.items++;
+    storage[someInstance.size() - 1] = value;
   };
-
-  someInstance.dequeue = function() {
+//  console.log('storage', storage);
+  someInstance.pop = function() {
+    if (someInstance.items === 0) {
+      return 0;
+    } else {
+      var popped = storage[someInstance.size() - 1];
+      delete storage[someInstance.size() - 1];
+      someInstance.items--;
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    return someInstance.items;
   };
-
+  
+    // console.log(someInstance.size().toString());
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,33 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+    'items' : 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // var test = someInstance.size().toString();
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.items++;
+    storage[someInstance.size() - 1] = value;
   };
-
+//  console.log('storage', storage);
   someInstance.pop = function() {
+    if (someInstance.items === 0) {
+      return 0;
+    } else {
+      var popped = storage[someInstance.size() - 1];
+      delete storage[someInstance.size() - 1];
+      someInstance.items--;
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    return someInstance.items;
   };
-
+  
+    // console.log(someInstance.size().toString());
   return someInstance;
 };
