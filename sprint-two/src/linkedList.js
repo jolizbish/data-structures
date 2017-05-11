1102ca58c77367bc775ba818995361542da9838a
Hack Reactor Student <student@hackreactor.com>
2017-05-10 20:00:13 -0500

    Minor progress on linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,27 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    list.tail = Node(value);
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    } else {
+      var oldHead = list.head;
+      list.head = Node(list.head.next);
+      return oldHead;
+    }
   };
 
   list.contains = function(target) {
+    for (var key in list) {
+      if (list[key] === target) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   };
 
   return list;
