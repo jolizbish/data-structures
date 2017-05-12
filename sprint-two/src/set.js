3912d98fe072260fefee7d064356541ff77840a1
Hack Reactor Student <student@hackreactor.com>
2017-05-11 19:14:17 -0500

    Implement set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this.storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  var isInSet = false;
+  for (var i = 0; i < this.storage.length; i++) {
+    if (item === this.storage[i]) {
+      isInSet = true;
+    }
+  }
+  return isInSet;
 };
 
 setPrototype.remove = function(item) {
+  var index;
+  for (var i = 0; i < this.storage.length; i++) {
+    if (item === this.storage[i]) {
+      index = i;
+    }
+  }
+  this.storage.splice(index, 1);
 };
 
 /*
