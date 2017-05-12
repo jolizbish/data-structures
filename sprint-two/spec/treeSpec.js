e6739c5cfa7d7051556d067e12996a0ab24966e7
Hack Reactor Student <student@hackreactor.com>
2017-05-12 16:18:00 -0500

    Add additional test for treeSpec

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -40,5 +40,17 @@ describe('tree', function() {
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
   });
+  
+  it('should correctly detect doubly-nested children', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    tree.children[0].children[0].addChild(9);
+    expect(tree.contains(7)).to.equal(true);
+    expect(tree.contains(8)).to.equal(true);
+    expect(tree.contains(9)).to.equal(true);
+
+  });
 
 });
